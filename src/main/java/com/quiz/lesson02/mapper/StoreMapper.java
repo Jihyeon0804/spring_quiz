8ca44df5cbef5043ee1359a3b061269153532a3b
학원 <test@test.com>
2023-09-21 21:46:45 +0900

    Spring DB 연동 2

---
+++ src/main/java/com/quiz/lesson02/mapper/StoreMapper.java
@@ -0,0 +1,15 @@
+package com.quiz.lesson02.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.quiz.lesson02.domain.Store;
+
+@Repository	// MyBatis가 DB와 연동되는 클래스를 Spring bean 으로 만들어주는 어노테이션
+public interface StoreMapper {
+	
+	// input : X
+	// output : List<Store>
+	public List<Store> selectStoreList();
+}
